c553070d0af63b5f1f5838ffa3637dab3be5760b
danguepre <danguepre@alum.us.es>
2023-03-23 00:04:14 +0100

    Added-- Task 012 danguepre

--- src/main/java/acme/features/authenticated/student/AuthenticatedStudentController.java
+++ src/main/java/acme/features/authenticated/student/AuthenticatedStudentController.java
@@ -27,7 +27,10 @@ public class AuthenticatedStudentController extends AbstractController<Authentic
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
-	protected AuthenticatedStudentCreateService createService;
+	protected AuthenticatedStudentCreateService	createService;
+
+	@Autowired
+	protected AuthenticatedStudentUpdateService	updateService;
 
 	// Constructors -----------------------------------------------------------
 
@@ -35,6 +38,7 @@ public class AuthenticatedStudentController extends AbstractController<Authentic
 	@PostConstruct
 	protected void initialise() {
 		super.addBasicCommand("create", this.createService);
+		super.addBasicCommand("update", this.updateService);
 	}
 
 }
