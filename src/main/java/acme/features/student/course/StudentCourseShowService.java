0ac92af76e14496320cb35bc1e9689570a3fa6a4
danguepre <danguepre@alum.us.es>
2023-04-20 03:06:50 +0200

    Completed --Task 013 danguepre

--- src/main/java/acme/features/student/course/StudentCourseShowService.java
+++ src/main/java/acme/features/student/course/StudentCourseShowService.java
@@ -1,3 +1,14 @@
+/*
+ * StudentCourseShowService.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
 
 package acme.features.student.course;
 
@@ -52,7 +63,7 @@ public class StudentCourseShowService extends AbstractService<Student, Course> {
 		assert object != null;
 		Tuple tuple;
 		tuple = super.unbind(object, "code", "title", "courseAbstract", "courseType", "retailPrice", "link");
-		//tuple.put("lecture", object.getLecturer().getAlmaMater());
+		tuple.put("lecturer", object.getLecturer().getAlmaMater());
 		super.getResponse().setData(tuple);
 	}
 }
